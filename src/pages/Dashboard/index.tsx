dea81b3aba3f4a65da7779538e0d44c599db0ab9
André Lusegardis <lusegardis@gmail.com>
2021-02-18 17:20:43 -0300

    CSS & TSX done

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -5,10 +5,55 @@ import { Container } from './styles';
 const Dashboard: React.FC = () => (
   <>
     <Container>
-      <h1>Hello World</h1>
-      <p>Template</p>
-      <br />
-      <a href="Page2">Sign up</a>
+      <h2>Animated Collapsibles</h2>
+
+      <p>A Collapsible:</p>
+      <button type="button" className="collapsible">
+        Open Collapsible
+      </button>
+      <div className="content">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+
+      <p>Collapsible Set:</p>
+      <button type="button" className="collapsible">
+        Open Section 1
+      </button>
+      <div className="content">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+      <button type="button" className="collapsible">
+        Open Section 2
+      </button>
+      <div className="content">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+      <button type="button" className="collapsible">
+        Open Section 3
+      </button>
+      <div className="content">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
     </Container>
   </>
 );
